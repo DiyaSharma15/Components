98b377bcb768b136d9a83c0130f9d72dc284e7ae
DiyaSharma15 <harry.diya161@gmail.com>
2023-11-02 23:42:51 -0600

    components created and imported

---
+++ components/ToDoForm.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native'
+
+export default function ToDoForm() {
+    return(
+        <View style={styles.form}>
+        <TextInput
+            style={styles.input}
+            placeholder="Add as new task..."
+        />
+        <Button title="Add" />
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    form: {
+        flexDirection: 'row',
+        justifyContent: 'space-between',
+        alignItems: 'center',
+        marginHorizontal: 20,
+        marginTop: 20,
+    },
+})
